260a367547d86bb96b4122d3115843d982c9c7a0
Natali <nevro1308@gmail.com>
2023-02-10 15:10:28 +0200

    added first home work

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "../App.module.css";
+
+export default function Header() {
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.header_wrap}>
+                    <div>
+                        <a>Docs</a>
+                    </div>
+                    <div>
+                        <a>Tutorial</a>
+                    </div>
+                    <div>
+                        <a>Blog</a>
+                    </div>
+                    <div>
+                        <a>Community</a>
+                    </div>
+                    <div>
+                        <input placeholder="Search..."></input>
+                    </div>
+                    <div>
+                        <button>LOG IN</button>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../App.module.css";
+
+export default function Main() {
+    return (
+        <>
+            <div>
+                <h1 className={styles.h1}>This is Content Area</h1>
+            </div>
+        </>
+    );
+}
